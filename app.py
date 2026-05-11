26ef0453c791907b05b3dac4fa5fb4c5bac049f4
anshulbisen06-png <anshulbisen06@gmail.com>
2026-05-11 05:45:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
+import streamlit as st
 
+st.set_page_config(page_title="Leaf AI", page_icon="🌿")
+
+st.title("🌿 AI Leaf Health System")
+
+st.success("Project Running Successfully")
+
+uploaded_file = st.file_uploader(
+    "Upload Leaf Image",
+    type=["jpg","png","jpeg"]
+)
+
+if uploaded_file:
+    st.image(uploaded_file)
+    st.write("Disease Prediction: Healthy Leaf")
+    st.write("Confidence Score: 96%")
 print("AI Leaf Health System Ready")
